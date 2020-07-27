b04830e15a9b392644d1a28816cdb749ff55d205
Prashanthvara <44000766+Prashanthvara@users.noreply.github.com>
2020-07-27 11:06:59 -0400

    Delete _navigation.php

--- _navigation.php
+++
@@ -1,7 +0,0 @@
-            <div class="space">
-                <a href="index.php">Student</a>
-                |
-                <a href="doctor.php">Doctor</a>
-                |
-                <a href="manager.php">Manager</a>
-            </div>
