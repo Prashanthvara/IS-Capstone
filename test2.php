580738ed0b3abd729022a10a6ede528798afee00
Prashanthvara <44000766+Prashanthvara@users.noreply.github.com>
2020-07-27 11:05:43 -0400

    Delete test2.php

--- test2.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once('connection.php');
-$sql1 = "UPDATE appointment SET appointment_status = \"free\" WHERE appointment_id=".$_GET['view_id'];
-$result = mysqli_query($db,$sql1)
-?>
-
